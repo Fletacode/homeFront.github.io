a50a4c686a6084c5bbf90a72769c2e73154cdaaf
Fletacode <88104352+Fletacode@users.noreply.github.com>
2023-09-07 11:46:35 +0000

    Start! Fixes #1

--- home/src/routers/Main.js
+++ home/src/routers/Main.js
@@ -6,8 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Main() {
 
   return (
-    <>	
-	 
+    <>
+	 //드디어 git추가
 	 <Carousel style={{padding:'20px'}}>
 	   <Carousel.Item>
         <Image src="https://homeservice.run.goorm.site/images/TempProfile.jpg" 
